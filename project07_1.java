50e747e990b3fa307b7ca67f70d9b14a7e87c6e5
cvetan1234 <72192783+cvetan1234@users.noreply.github.com>
2020-10-01 15:44:19 +0300

    add project07_1######1_10_220

--- project07_1.java
+++ project07_1.java
@@ -1,5 +1,18 @@
 package edu.smg;
 
-public class project07_1 {
+import java.util.Scanner;
 
+public class project07_1 {
+	 public static void main(String[] args){
+		 Scanner in = new Scanner(System.in);
+		 System.out.println("Въведи 1-ви стринг!");
+		 String word1 = in.nextLine();
+		 System.out.println("Въведи 2-ри стринг!");
+		 String word2 = in.nextLine();
+		 boolean ready = false;
+		 while (ready == false){
+			 
+		 }
+		 
+	 }
 }
