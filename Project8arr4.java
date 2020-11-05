b936b57e561b2828b415ba1ff7b5bc70195a264e
cvetan1234 <72192783+cvetan1234@users.noreply.github.com>
2020-11-05 15:48:16 +0200

    Add files via upload

--- Project8arr4.java
+++ Project8arr4.java
@@ -1,8 +1,74 @@
 package smg;
 import java.util.Scanner;
+
 public class Project8arr4 {
-	public static void main(String[] args) {
+	static int element_On_Position(int a, int position) {
+		a = (int) (a / Math.pow(10, (position-1)));
+		return a%10;
+	}
+	static boolean is_It_in_the_array_9(int c, int[] a) {
+		boolean is_It_in_the_array = false;
+		for (int r = 0;r<9;r++) {
+			if (a[r] != c) {
+				is_It_in_the_array = true;
+			}
+		}
+		return is_It_in_the_array;
+	}
+	static boolean valid(int[] a) {
+		boolean is_It_valid = true;
+		if (a.length != 9) {
+			return false;
+		}
+		for (int v = 0;v < 9;v++) {
+			if (is_It_in_the_array_9(v,a) != true) {
+				is_It_valid = false;
+			}
+		}
+		return is_It_valid;
 		
 	}
-
+	static void text(String a) {
+		System.out.println(a);
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int input;
+		boolean is_It_Sudoku = true;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("SUDOKU: ");
+		int[][] sudoku = new int[9][9];
+		for (int i = 0; i < 9; i++) {
+			input = sc.nextInt();
+			for (int j = 0;j<9;j++) {
+				sudoku[i][j] = element_On_Position(input,(9-j));
+			}
+		}
+		//for (int i = 0;i < 9;i++) {
+		//	for (int j = 0;j < 9;j++) {
+		//		text("Type a number for row:" + (i+1) + " column:" + (j+1));
+		//		sudoku[i][j] = sc.nextInt();
+		//	}
+		//}
+		int[] a = new int[9];
+		for (int j = 0;j<9;j++) {
+			for(int q = 0;q<9;q++) {
+				a[q] = sudoku[j][q];
+				if (valid(a) != true) {
+					is_It_Sudoku = false; break;
+				}
+			}
+		}
+		for (int j = 0;j<9;j++) {
+			for(int q = 0;q<9;q++) {
+				a[q] = sudoku[q][j];
+				if (valid(a) != true) {
+					is_It_Sudoku = false; break;
+				}
+			}
+		}
+		System.out.println(is_It_Sudoku);
+	}
 }
+
